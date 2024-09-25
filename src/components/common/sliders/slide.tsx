5f81b364323dd454f5d97b37654486c79ba839f1
Ayesha Muzamil <ayesha.muzammil@funavry.com+1-at-890242958078>
2024-09-25 17:36:45 +0500

    dynamic home page

--- src/components/common/sliders/slide.tsx
+++ src/components/common/sliders/slide.tsx
@@ -1,6 +1,5 @@
-'use client'
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+'use client';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Icon } from '@iconify/react';
 import userimg from "../../../../public/assets/images/defaultuser.jpg";
@@ -9,22 +8,20 @@ import userimg from "../../../../public/assets/images/defaultuser.jpg";
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-// import required modules
+// Import required modules
 import { Navigation } from 'swiper/modules';
 import Image from 'next/image';
+import { promotedTE } from '@/services/helpers/staticdata';
 
 const Slider: React.FC = () => {
   return (
     <>
-      <Swiper navigation={true}
+      <Swiper 
+        navigation={true}
         slidesPerView={3}
         spaceBetween={30}
         slidesPerGroup={1}
         loop={true}
-        // navigation={{
-        //     prevEl: ".swiper-button-prev.swiper-button-disabled",
-        //     nextEl: ".swiper-button-next.swiper-button-disabled",
-        // }}
         breakpoints={{
           320: {
             slidesPerView: 1,
@@ -47,244 +44,65 @@ const Slider: React.FC = () => {
             spaceBetween: 30
           }
         }}
-        modules={[Navigation]} className="mySwiper">
-
+        modules={[Navigation]}
+        className="mySwiper"
+      >
         <div className="row">
-          <SwiperSlide>
-            <div className="col">
-              <div className="promoted_card mb-2 position-relative">
-                <div className="ribbon-1">
-                  <Image
-                    src="/assets/images/promote.svg"
-                    alt="img"
-                    className="img-fluid ribbon-img"
-                    width={255}
-                    height={255}
-                    priority
-                  />
-                </div>
-                <div className="ribbon-2"><span>Disability</span></div>
-                <div className="card_heading">
-                  <div className="userimg">
+          {promotedTE.map((data: any) => (
+            <SwiperSlide key={data.id}>
+              <div className="col">
+                <div className="promoted_card mb-2 position-relative">
+                  <div className="ribbon-1">
                     <Image
-                      src={userimg}
-                      width={48}
-                      height={48}
-                      alt="User Image"
+                      src="/assets/images/promote.svg"
+                      alt="img"
+                      className="img-fluid ribbon-img"
+                      width={255}
+                      height={255}
+                      priority
                     />
                   </div>
-                  <div className="usertext">
-                    <h5>Andrew Smith</h5>
-                    <h6>WordPress Developer</h6>
-                    <div className="rating">
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined">
-                        kid_star
-                      </span>
+                  {data.disability && <div className="ribbon-2"><span>Disability</span></div>}
+                  <div className="card_heading">
+                    <div className="userimg">
+                      <Image
+                        src={data.src}
+                        width={48}
+                        height={48}
+                        alt="User Image"
+                      />
+                    </div>
+                    <div className="usertext">
+                      <h5>{data.name}</h5>
+                      <h6>{data.designation}</h6>
+                      <div className="rating">
+                        {[...Array(5)].map((_, index) => (
+                          <span
+                            key={index}
+                            className={`material-symbols-outlined ${index < data.rating ? "rated" : ""}`}
+                          >
+                            kid_star
+                          </span>
+                        ))}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <p>
-                  I am Web developer expert with over eight years of experience
-                  in Websites... <a href="">more</a>
-                </p>
-                <div className="d-flex align-items-baseline justify-content-between">
-                  <h6 className="fs-12">85 Tasks</h6>
-                  <button className="btn btn-outline-info rounded-pill text-white fs-10 btn-sm">
-                    View Details <Icon icon="material-symbols:arrow-right-alt-rounded" />
-                  </button>
+                  <p>{data.description} </p>
+                  {/* <a href="#">more</a> */}
+                  <div className="d-flex align-items-baseline justify-content-between">
+                    <h6 className="fs-12">{data.tasks} Tasks</h6>
+                    <button className="btn btn-outline-info rounded-pill text-white fs-10 btn-sm">
+                      View Details <Icon icon="material-symbols:arrow-right-alt-rounded" />
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="col">
-              <div className="promoted_card mb-2 position-relative">
-                <div className="ribbon-1">
-                  <Image
-                    src="/assets/images/promote.svg"
-                    alt="img"
-                    className="img-fluid ribbon-img"
-                    width={255}
-                    height={255}
-                    priority
-                  />
-                </div>
-                <div className="card_heading">
-                  <div className="userimg">
-                    <Image
-                      src={userimg}
-                      width={48}
-                      height={48}
-                      alt="User Image"
-                    />
-                  </div>
-                  <div className="usertext">
-                    <h5>Andrew Smith</h5>
-                    <h6>WordPress Developer</h6>
-                    <div className="rating">
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined">
-                        kid_star
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <p>
-                  I am Web developer expert with over eight years of experience
-                  in Websites... <a href="">more</a>
-                </p>
-                <div className="d-flex align-items-baseline justify-content-between">
-                  <h6 className="fs-12">144 Tasks</h6>
-                  <button className="btn btn-outline-info rounded-pill text-white fs-10 btn-sm">
-                    View Details <Icon icon="material-symbols:arrow-right-alt-rounded" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="col">
-              <div className="promoted_card mb-2 position-relative">
-                <div className="ribbon-1">
-                  <Image
-                    src="/assets/images/promote.svg"
-                    alt="img"
-                    className="img-fluid ribbon-img"
-                    width={255}
-                    height={255}
-                    priority
-                  />
-                </div>
-                <div className="ribbon-2"><span>Disability</span></div>
-                <div className="card_heading">
-                  <div className="userimg">
-                    <Image
-                      src={userimg}
-                      width={48}
-                      height={48}
-                      alt="User Image"
-                    />
-                  </div>
-                  <div className="usertext">
-                    <h5>Andrew Smith</h5>
-                    <h6>WordPress Developer</h6>
-                    <div className="rating">
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined">
-                        kid_star
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <p>
-                  I am Web developer expert with over eight years of experience
-                  in Websites... <a href="">more</a>
-                </p>
-                <div className="d-flex align-items-baseline justify-content-between">
-                  <h6 className="fs-12">100 Tasks</h6>
-                  <button className="btn btn-outline-info rounded-pill text-white fs-10 btn-sm">
-                    View Details <Icon icon="material-symbols:arrow-right-alt-rounded" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="col">
-              <div className="promoted_card mb-2 position-relative">
-                <div className="ribbon-1">
-                  <Image
-                    src="/assets/images/promote.svg"
-                    alt="img"
-                    className="img-fluid ribbon-img"
-                    width={255}
-                    height={255}
-                    priority
-                  />
-                </div>
-                <div className="ribbon-2"><span>Disability</span></div>
-                <div className="card_heading">
-                  <div className="userimg">
-                    <Image
-                      src={userimg}
-                      width={48}
-                      height={48}
-                      alt="User Image"
-                    />
-                  </div>
-                  <div className="usertext">
-                    <h5>Andrew Smith</h5>
-                    <h6>WordPress Developer</h6>
-                    <div className="rating">
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined rated">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined">
-                        kid_star
-                      </span>
-                      <span className="material-symbols-outlined">
-                        kid_star
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <p>
-                  I am Web developer expert with over eight years of experience
-                  in Websites... <a href="">more</a>
-                </p>
-                <div className="d-flex align-items-baseline justify-content-between">
-                  <h6 className="fs-12">85 Tasks</h6>
-                  <button className="btn btn-outline-info rounded-pill text-white fs-10 btn-sm">
-                    View Details <Icon icon="material-symbols:arrow-right-alt-rounded" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </div>
       </Swiper>
     </>
   );
-}
+};
 
 export default Slider;
---
+++ src/services/helpers/staticdata.ts
@@ -0,0 +1,198 @@
+import { time } from "console"
+
+
+export const promotedTE = [{
+    id:1,
+    isPromoted: true,
+    disability: true,
+    name: "Andrew Smith",
+    designation: "Word Press Developer",
+    rating: 2,
+    description:"I am Web developer expert with over eight years of experience in Websites...",
+    tasks: 85,
+    src:'/assets/images/image_2024_09_25T06_35_41_485Z.png'
+},
+{
+    id:2,
+    isPromoted: true,
+    disability: false,
+    name: "Stacy Haru",
+    designation: "UI/UX designer",
+    rating: 3,
+    description:"I am highly organized expert with over eight years of experience in Websites...",
+    tasks: 124,
+    src:'/assets/images/image_2024_09_25T06_35_56_397Z.png'
+},
+{
+    id:3,
+    isPromoted: true,
+    disability: true,
+    name: "David Malan",
+    designation: "UI/UX designer",
+    rating: 3,
+    description:"I'm a highly organized and creative force in the world of UI/UX Designer with ....",
+    tasks: 124,
+    src:'/assets/images/image_2024_09_25T06_35_26_648Z.png'
+},
+{
+    id:4,
+    isPromoted: true,
+    disability: true,
+    name: "Stacy Haru",
+    designation: "UI/UX designer",
+    rating: 3,
+    description:"I am highly organized expert with over eight years of experience in Websites...",
+    tasks: 124,
+    src:'/assets/images/image_2024_09_25T06_35_26_648Z.png'
+}
+]
+export const promotedTasks = [{
+    id:0,
+    isPromoted: true,
+    designation: "Front END Developer",
+    workingSlot:"Full Time",
+    country: "PAK",
+    rate: "200",
+    rating: 3,
+    status: "online",
+    description:"Develop and implement user interfaces for websites and web applications.Ensure...",
+    task_age: 2,
+},
+{
+    id:1,
+    isPromoted: true,
+    designation: "LAW Officer (Contracts)",
+    workingSlot:"Full Time",
+    country: "Dubai",
+    rate: "150",
+    rating: 3,
+    status: "online",
+    description:"Conduct in-depth research on United States federal and state laws/regulat...",
+    task_age: 2,
+
+},
+{
+    id:2,
+    isPromoted: true,
+    designation: "Ai Engineer",
+    workingSlot:"Full Time",
+    country: "Iran",
+    rate: "120",
+    rating: 3,
+    status: "online",
+    description:"As an AI Engineer at MTBC CareCloud, you will be responsible for developing and im...",
+    task_age: 2,
+
+},
+{
+    id:3,
+    isPromoted: true,
+    designation: "Client Relationship Manager",
+    workingSlot:"Full Time",
+    country: "Pakistan",
+    rate: "140",
+    rating: 3,
+    status: "online",
+    description:"Responding to the clients queries and provide them with adequate technic...",
+    task_age: 2,
+
+    
+},
+{
+    id:4,
+    isPromoted: true,
+    designation: "Project Manager",
+    workingSlot:"Full Time",
+    country: "India",
+    rate: "180",
+    rating: 3,
+    status: "online",
+    description:"Lead and manage agile projects from initiation to completion, ensuring timely...",
+    task_age: 2,
+
+},
+{
+    id:5,
+    isPromoted: true,
+    designation: "Software Engineer",
+    workingSlot:"Full Time",
+    country: "Philippines",
+    rate: "100",
+    rating: 3,
+    status: "online",
+    description:"Ability to work on desktop windows applications and service oriented tecture...",
+    task_age: 2,
+
+}
+]
+export const category = [{
+    id:0,
+    categoryName : "Visual Design",
+    description: "Develop modern and unique user interface designs that captivate users and enhance work",
+    isDark: true,
+
+},
+{
+    id:1,
+    categoryName : "Quality Assurance",
+    description: "Ensure the highest standards by thoroughly testing products to identify and resolve defects",
+    isDark: false,
+
+},
+{
+    id:2,
+    categoryName : "Marketing",
+    description: "Create impactful strategies that connect with audiences, promoting products and services.",
+    isDark: false,
+
+},
+{
+    id:3,
+    categoryName : "Artificial Intelligence",
+    description: "Develop intelligent systems that automate processes and provide innovative solutions accordingly",
+    isDark: false,
+
+},
+{
+    id:4,
+    categoryName : "Web Development",
+    description: "Build responsive and innovative websites using the latest technologies to meet modern",
+    isDark: false,
+
+},
+{
+    id:5,
+    categoryName : "Project Management",
+    description: "Manage projects efficiently by applying structured methodologies to deliver results on",
+    isDark: false,
+
+}
+]
+
+export const clientTest = [{
+    name : "Robert Fox",
+    id: 0,
+    designation : "UI/UX Designer",
+    rating : 3,
+    src:'/assets/images/image_2024_09_25T06_35_41_485Z.png',
+    description: "Exceptional work! The team delivered a seamless, user-friendly application that exceeded our expectations"
+},
+{
+    name : "Bessie Cooper",
+    id:1,
+    designation : "Creative Director",
+    rating : 3,
+    src:'/assets/images/image_2024_09_25T06_35_56_397Z.png',
+    description: "Highly professional and efficient. The project was completed on time, with excellent communication throughout"
+},
+{
+    name : "Jane Cooper",
+    id: 2,
+    designation : "Photographer",
+    rating : 3,
+    src:'/assets/images/image_2024_09_25T06_35_26_648Z.png',
+    description: "Great collaboration! The attention to detail and commitment to quality truly stood out in every phase of the project"
+},
+
+
+]

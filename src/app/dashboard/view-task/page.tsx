5b1e9d80d4c6c9df446468c22e05ae4afa21fb60
UsmanButt <usman.asif@funavry.com>
2024-10-01 10:18:58 +0500

    changes

--- src/app/dashboard/view-task/page.tsx
+++ src/app/dashboard/view-task/page.tsx
@@ -1,10 +1,12 @@
 import ViewTasks from '@/components/viewTasks'
+import { Addtask } from '@/components/viewTasks/Form/Addtask'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <ViewTasks />
+      {/* <ViewTasks /> */}
+      <Addtask/>
     </div>
   )
 }
---
+++ src/components/viewTasks/Form/Addtask.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Addtask = () => {
+    return (
+        <section className='addtask'>
+            <div className="card text-center">
+                <div className="card-header bg-dark">
+                    Featured
+                </div>
+                <div className="card-body bg-gray">
+                    <h5 className="card-title">Special title treatment</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+                <div className="card-footer text-body-secondary">
+                    2 days ago
+                </div>
+            </div>
+        </section>
+    )
+}

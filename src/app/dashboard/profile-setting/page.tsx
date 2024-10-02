44f85feb6069ba39b5eb9e738b575f379e248dcd
muhammad.irfan@funavry.com <muhammad.irfan@funavry.com>
2024-10-02 11:54:49 +0500

    profile setting page design

---
+++ src/app/dashboard/profile-setting/page.tsx
@@ -0,0 +1,10 @@
+import ProfileSetting from '@/components/dashboardComponents/profileSetting/ProfileSetting'
+import React from 'react'
+
+const page = () => {
+    return (
+        <ProfileSetting />
+    )
+}
+
+export default page

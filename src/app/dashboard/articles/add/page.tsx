3346b56228142720503af9a77b0af6f01547c58b
tehreem.fatima <tehreem.fatima@funavry.com-at-890242958078>
2024-10-08 16:08:23 +0500

    pages added

---
+++ src/app/dashboard/articles/add/page.tsx
@@ -0,0 +1,12 @@
+import Newarticle from '@/components/dashboardComponents/talentxpertEX/Newarticle'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <Newarticle />
+        </div>
+    )
+}
+
+export default page
--- src/app/dashboard/articles/page.tsx
+++ src/app/dashboard/articles/page.tsx
@@ -1,9 +1,10 @@
+import { Articlelist } from '@/components/dashboardComponents/talentxpertEX/Articlelist'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <Articlelist />
     </div>
   )
 }

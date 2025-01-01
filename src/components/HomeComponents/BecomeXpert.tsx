149e87b195b4a3c584bbf06388b4751bb242d9c4
tehreem.fatima <tehreem.fatima@funavry.com-at-890242958078>
2025-01-01 11:07:32 +0500

    index page fixes

--- src/components/HomeComponents/BecomeXpert.tsx
+++ src/components/HomeComponents/BecomeXpert.tsx
@@ -1,40 +1,45 @@
 import { Icon } from '@iconify/react/dist/iconify.js'
+import Link from 'next/link'
 import React from 'react'
 
 const BecomeXpert = () => {
   return (
     <section className="become-section py-5">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-6 mb-2">
-          <div className="card bg-become-expert">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-8">
-                  <h5 className="fw-bold become-text">Become a TalentedXpert</h5>
-                  <p className='line-clamp-2'>Master your craft by honing your skills, staying updated, and providing expert solutions in your field</p>
-                  <button className="btn btn-info rounded-pill become-btn">Register Now <Icon icon="material-symbols:arrow-right-alt-rounded" /></button>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 mb-2">
+            <div className="card bg-become-expert">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-8">
+                    <h5 className="fw-bold become-text">Become a TalentedXpert</h5>
+                    <p className='line-clamp-2'>Master your craft by honing your skills, staying updated, and providing expert solutions in your field</p>
+                    <Link href={'/register'} className="btn btn-info rounded-pill become-btn">
+                      Register Now <Icon icon="material-symbols:arrow-right-alt-rounded" />
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-lg-6 mb-2">
-          <div className="card bg-become-requester">
-            <div className="card-body">
-              <div className="row text-light">
-                <div className="col-8">
-                  <h5 className="fw-bold become-text">Become a TalentRequester</h5>
-                  <p className="text-light line-clamp-2 ">Improve your ability to articulate needs clearly, request relevant information, and effectively collaborate</p>
-                  <button className="btn btn-info rounded-pill become-btn">Register Now <Icon icon="material-symbols:arrow-right-alt-rounded" /></button>
+          <div className="col-lg-6 mb-2">
+            <div className="card bg-become-requester">
+              <div className="card-body">
+                <div className="row text-light">
+                  <div className="col-8">
+                    <h5 className="fw-bold become-text">Become a TalentRequester</h5>
+                    <p className="text-light line-clamp-2 ">Improve your ability to articulate needs clearly, request relevant information, and effectively collaborate</p>
+                    <Link href={'/register'} className="btn btn-info rounded-pill become-btn">
+                      Register Now <Icon icon="material-symbols:arrow-right-alt-rounded" />
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   )
 }
 
--- src/components/HomeComponents/MainDescription.tsx
+++ src/components/HomeComponents/MainDescription.tsx
@@ -8,58 +8,46 @@ import ImageFallback from '../common/ImageFallback/ImageFallback';
 const MainDescription = () => {
   return (
     <section className="herosection py-5">
-          <div className="container">
-            <div className="heroimg">
-              <ImageFallback src={heroimg} alt="Heroimg" className="hero-img"/>
+      <div className="container">
+        <div className="heroimg">
+          <ImageFallback src={heroimg} alt="Heroimg" className="hero-img" />
+        </div>
+        <div className="hero-content">
+          <div className="d-flex justify-content-center flex-wrap">
+            <div className="herocard">
+              <h1>Why TalentedXpert?</h1>
+              <p className='line-clamp-3'>
+                Because we deliver exceptional talent quickly and efficiently..
+              </p>
+              <a href="#">
+                Read more
+                <Icon icon="line-md:arrow-right" />
+              </a>
             </div>
-            <div className="hero-content">
-              <div className="d-flex justify-content-center flex-wrap">
-                <div className="herocard">
-                  <h1>Why TalentedXpert?</h1>
-                  <p className='line-clamp-3'>
-                  Because we deliver exceptional talent quickly and efficiently..
-                  </p>
-                  <a href="#">
-                    Read more
-                    {/* <span className="material-symbols-outlined">
-                      arrow_forward
-                    </span> */}
-                    <Icon icon="line-md:arrow-right" />
-                  </a>
-                </div>
-                <div className="v-divider d-block"></div>
-                <div className="herocard">
-                  <h1>What is TalentedXpert?</h1>
-                  <p className='line-clamp-3'>
-                  TalentedXpert connects businesses with top-tier talent, providing..
-                  </p>
-                  <a href="#">
-                    Read more
-                    {/* <span className="material-symbols-outlined">
-                      arrow_forward
-                    </span> */}
-                    <Icon icon="line-md:arrow-right" />
-                  </a>
-                </div>
-              </div>
-              <div className="buttons">
-              <Link
-                  className="btn btn-dark rounded-pill hero-btn"
-                  href={'/talented-xperts'}>Find your TalentedXperts</Link>
-                {/* <button className="btn btn-dark rounded-pill hero-btn" >
-                  Find your TalentedXperts
-                </button> */}
-                <Link
-                  className="btn btn-light rounded-pill hero-btn find-yournext-task-btn"
-                  href={'/tasks'}>Find your next Task</Link>
-                {/* <button className="btn btn-light rounded-pill hero-btn">
-                  Find your next Task
-                </button> */}
-                <Link className="btn btn-info rounded-pill hero-btn" href={'/register'} >Join Us Now</Link>
-              </div>
+            <div className="v-divider d-block"></div>
+            <div className="herocard">
+              <h1>What is TalentedXpert?</h1>
+              <p className='line-clamp-3'>
+                TalentedXpert connects businesses with top-tier talent, providing..
+              </p>
+              <a href="#">
+                Read more
+                <Icon icon="line-md:arrow-right" />
+              </a>
             </div>
           </div>
-        </section>
+          <div className="buttons">
+            <Link
+              className="btn btn-dark rounded-pill hero-btn"
+              href={'/talented-xperts'}>Find your TalentedXperts</Link>
+            <Link
+              className="btn btn-light rounded-pill hero-btn find-yournext-task-btn"
+              href={'/tasks'}>Find your next Task</Link>
+            <Link className="btn btn-info rounded-pill hero-btn" href={'/register'} >Join Us Now</Link>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 

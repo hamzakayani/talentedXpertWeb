e0be5b73575367e5dba155eb053a3abe53121cbe
tehreem.fatima <tehreem.fatima@funavry.com-at-890242958078>
2025-01-02 10:54:40 +0500

    fix text editor spacing

--- src/components/common/TextEditor/TextEditor.tsx
+++ src/components/common/TextEditor/TextEditor.tsx
@@ -3,12 +3,38 @@ import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
 
 const TextEditor: React.FC<any> = ({ value, setValue, className, style, placeholder }) => {
+    const toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        ['link'],
+      
+        // [{ 'header': 5 }, { 'header': 6 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+      
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [5, 6, false] }],
+      
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+      
+        ['clean']                                         // remove formatting button
+    ]
+
+    const modules = {
+        toolbar: toolbarOptions
+    };
+
     return (
         <div className={`${className}`} style={style}>
             <ReactQuill
                 value={value}
                 onChange={(value) => setValue(value)}                  
                 theme="snow"
+                // modules={modules} 
                 placeholder={placeholder}
             />
         </div>

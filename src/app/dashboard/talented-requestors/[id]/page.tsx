50fb2b19f5315e937d533ff337780e66d0cf1eec
tehreem.fatima <tehreem.fatima@funavry.com-at-890242958078>
2025-01-13 11:41:42 +0500

    feat: add view profile page in talent requestor

---
+++ src/app/dashboard/talented-requestors/[id]/page.tsx
@@ -0,0 +1,13 @@
+import ViewProfile from '@/components/dashboardComponents/viewProfile/viewProfile'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <ViewProfile />
+    </div>
+
+  )
+}
+
+export default page
--- src/app/dashboard/talented-requestors/page.tsx
+++ src/app/dashboard/talented-requestors/page.tsx
@@ -1,4 +1,3 @@
-import MainLayout from '@/components/MainLayout'
 import Talentedxperts from '@/components/talentedxperts/Talentedxperts'
 import React from 'react'
 

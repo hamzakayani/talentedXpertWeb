34e401180ab90263172fe18f84d4f121b1e479de
tehreem.fatima <tehreem.fatima@funavry.com-at-890242958078>
2025-02-24 11:34:07 +0500

    socket functionality

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -4,45 +4,56 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux'; 
 import { toast } from 'react-toastify';
+import { Socket } from 'socket.io-client';
 
 const useSocket = () => {
     const dispatch = useDispatch();
     const token = useSelector((state: RootState) => state.auth.token); 
     const user = useSelector((state: RootState) => state.user);
 
+    const [socket, setSocket] = useState<Socket | null>(null);
+
     useEffect(() => {
         if (token && user) {
-            const socket = getSocket(token, user?.profile?.length > 0 && user?.profile[0]?.id);
+            // const socket = getSocket(token, user?.profile?.length > 0 && user?.profile[0]?.id);
+            const profileId = user?.profile?.length > 0 && user?.profile[0]?.id;
 
-            if(socket){
-                const notificationHandler = (notification: any) => {
-                    console.log(">>>>", notification);
-    
-                    // toast(notification.message, {
-                    //     type: notification.type || 'info',  
-                    //     position: toast.POSITION.TOP_RIGHT,
-                    //     autoClose: 5000, // Auto close after 5 seconds
-                    // });
-                };
+            if(!socket){
+                const newSocket = getSocket(token, profileId);
                 
-                // socket.send("Hello!");
-                socket.emit('notification', { message: 'hello world!' })
+                if(newSocket){
+                    setSocket(newSocket)
+                    const notificationHandler = (notification: any) => {
+                        console.log(">>>>", notification);
+        
+                        // toast(notification.message, {
+                        //     type: notification.type || 'info',  
+                        //     position: toast.POSITION.TOP_RIGHT,
+                        //     autoClose: 5000, // Auto close after 5 seconds
+                        // });
+                    };
+                    
+                    console.log(socket)
+        
+                    newSocket.on("notification", notificationHandler);
     
-                socket.on("notification", notificationHandler);
-
-                return () => {
-                    socket.off("notification", notificationHandler);
-                    closeSocket();
-                };
+                    return () => {
+                        newSocket.off("notification", notificationHandler);
+                        closeSocket();
+                    };
+                }
             }
 
             return () => {
                 closeSocket();
             };
         }
+        return () => {
+            closeSocket();
+        };
     }, [token, user, dispatch]);
 
-    // return { token };
+    return { socket };
 };
 
 export default useSocket;
--- src/services/utils/socket.ts
+++ src/services/utils/socket.ts
@@ -5,10 +5,17 @@ let socket: Socket | null = null;
 
 export const getSocket = (token: string | null, id: number) => {
     if (!socket && token && id) {
-        socket = io(`${SOCKET_URL}?profileId=${id}`);
+        socket = io(`${SOCKET_URL}`,{
+            transports: ['websocket'],
+            query: {
+                profileId: id
+            }
+        });
 
         socket.on("connect", () => {
             console.log("Connected to socket server");
+            // socket?.emit('notification', { message: 'hello world!' })
+            // console.log("Notification send");
         });
 
         socket.on("disconnect", () => {

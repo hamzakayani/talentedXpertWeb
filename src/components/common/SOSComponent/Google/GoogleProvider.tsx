5dc0b7210f9467b1be5bfeb27463cd3277f2a495
Muhammad Naeem <muhammad.naeem@funavry.com>
2025-09-03 18:47:57 +0500

    Enhance login and registration UI components with new styles and functionality
    - Added styles for login and registration pages in mainstyle.scss
    - Updated Header component for improved layout and logo dimensions
    - Refactored RegisterComponent to include social login buttons and a new layout
    - Improved Google and LinkedIn button components for better user experience
    - Enhanced Signin component with new form structure and icons
    - Implemented responsive design adjustments across components

--- src/components/common/SOSComponent/Google/GoogleProvider.tsx
+++ src/components/common/SOSComponent/Google/GoogleProvider.tsx
@@ -1,15 +1,17 @@
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import React from 'react'
-import GoogleBtn from './GoogleBtn'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import React from "react";
+import GoogleBtn from "./GoogleBtn";
 
 const GoogleProvider = ({ profileType }: { profileType: string }) => {
-    return (
-        <>
-            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENTID as string}>
-                <GoogleBtn profileType={profileType} />
-            </GoogleOAuthProvider>
-        </>
-    )
-}
+  return (
+    <>
+      <GoogleOAuthProvider
+        clientId={process.env.REACT_APP_GOOGLE_CLIENTID as string}
+      >
+        <GoogleBtn profileType={profileType} />
+      </GoogleOAuthProvider>
+    </>
+  );
+};
 
-export default GoogleProvider
+export default GoogleProvider;

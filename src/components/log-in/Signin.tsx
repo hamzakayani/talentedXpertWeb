9ab44755a75ec9c971a5c1ba5df870a68cc76c00
Ayesha321-dot <ayesha.muzammil@funavry.com>
2025-09-04 14:01:08 +0500

    button size

--- src/components/log-in/Signin.tsx
+++ src/components/log-in/Signin.tsx
@@ -60,7 +60,7 @@ const Signin = () => {
       }`}
       onClick={onClick}
       style={{
-        height: "48px",
+        height: "35px",
         width: "50%",
         borderRadius: "8px",
         border: isActive ? "none" : "1px solid #000",
@@ -114,7 +114,7 @@ const Signin = () => {
                   isActive={loginAs === "TE"}
                   onClick={() => setValue("loginAs", "TE", { shouldValidate: true })}
                   icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none">
                       <path d="M6.57757 15.4816C5.1628 16.324 1.45336 18.0441 3.71266 20.1966C4.81631 21.248 6.04549 22 7.59087 22H16.4091C17.9545 22 19.1837 21.248 20.2873 20.1966C22.5466 18.0441 18.8372 16.324 17.4224 15.4816C14.1048 13.5061 9.89519 13.5061 6.57757 15.4816Z" stroke={loginAs === "TE" ? "#fff" : "#000"} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                       <path d="M16.5 6.5C16.5 8.98528 14.4853 11 12 11C9.51472 11 7.5 8.98528 7.5 6.5C7.5 4.01472 9.51472 2 12 2C14.4853 2 16.5 4.01472 16.5 6.5Z" stroke={loginAs === "TE" ? "#fff" : "#000"} strokeWidth="1.5" />
                     </svg>
@@ -125,7 +125,7 @@ const Signin = () => {
                   isActive={loginAs === "TR"}
                   onClick={() => setValue("loginAs", "TR", { shouldValidate: true })}
                   icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none">
                       <path d="M3 7.5C3 5.01472 5.01472 3 7.5 3H16.5C18.9853 3 21 5.01472 21 7.5V16.5C21 18.9853 18.9853 21 16.5 21H7.5C5.01472 21 3 18.9853 3 16.5V7.5Z" stroke={loginAs === "TR" ? "#fff" : "#000"} strokeWidth="1.5" />
                       <path d="M8 12H16M8 8H16M8 16H12" stroke={loginAs === "TR" ? "#fff" : "#000"} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>

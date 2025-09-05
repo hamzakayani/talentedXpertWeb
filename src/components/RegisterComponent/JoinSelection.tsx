3a26b66f1db9ea71747cc31917c104e03c956f80
appcrafter007 <junaidhayat18@yahoo.com>
2025-09-05 15:04:28 +0500

    textual changes

--- src/components/RegisterComponent/JoinSelection.tsx
+++ src/components/RegisterComponent/JoinSelection.tsx
@@ -18,7 +18,7 @@ export default function JoinSelection({activeStep, setActiveStep}: {activeStep: 
       <div className="container">
         <div className="row">
           <div className="col-12 col-lg-5 mx-auto">
-            <h2 className="text-center mb-4 text-medium">Join as Xpert or Requestor</h2>
+            <h2 className="text-center mb-4 text-medium">Join as TalentedXpert or TalentRequestor</h2>
             <div className="d-flex gap-2 mb-3 justify-content-center">
                 <UserTypeButton
                     label="Individual"
@@ -84,7 +84,7 @@ export default function JoinSelection({activeStep, setActiveStep}: {activeStep: 
                     </div>
                     <div className="d-flex gap-2">
                     <div>
-                        <p className="mb-0 fw-medium">I&apos;m a requestor,</p>
+                        <p className="mb-0 fw-medium">I&apos;m a Talentrequestor,</p>
                         <p className="mb-0 fw-medium">hiring for a project</p>
                     </div>
                     </div>
@@ -123,7 +123,7 @@ export default function JoinSelection({activeStep, setActiveStep}: {activeStep: 
                 </div>
                 <div className="d-flex gap-2">
                     <div>
-                        <p className="mb-0 fw-medium">I&apos;m a Xpert,</p>
+                        <p className="mb-0 fw-medium">I&apos;m a TalentedXpert,</p>
                         <p className="mb-0 fw-medium">looking for work</p>
                     </div>
                 </div>

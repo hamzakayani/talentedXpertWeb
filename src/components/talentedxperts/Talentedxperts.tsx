8738b0562361fde6a13836fa99bf977d20491693
TehreemFatima-dev <tehreem.fatima@funavry.com>
2025-09-30 17:12:58 +0500

    bug fix

--- src/components/talentedxperts/Talentedxperts.tsx
+++ src/components/talentedxperts/Talentedxperts.tsx
@@ -152,7 +152,7 @@ const Talentedxperts: FC<any> = ({ isDashboard }) => {
                     {fetchUsers?.isLoading && <SpinnerLoader />}
                     {!fetchUsers?.isLoading && fetchUsers?.data?.data?.users?.length > 0 ? fetchUsers?.data?.data?.users?.map((data:any) => (
                         <div className='col-md-4' key={data.id}>
-                            <PromotedTaskCard data={data} activeTab={userType === 'talent-requestors' ? "talentrequestor" : "talentedxpert"} isDark={true} btn={"View Details"} isDashboard={true}  />
+                            <PromotedTaskCard data={data} activeTab={userType === 'talent-requestors' ? "talentrequestor" : "talentedxpert"} isDark={true} btn={"View Details"} isDashboard={isDashboard}  />
                         </div>
                     )) : !fetchUsers?.isLoading && <NoFound className={"col-12 text-center mb-3"} message="No user found" isDark={true} />}
                 </div>
